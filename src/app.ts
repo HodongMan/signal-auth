b196a4c116e3933a63a99a6cd76fd4af43843938
hodongman <jhd9206@gmail.com>
2018-07-27 01:05:56 +0900

    routes update

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,17 @@
 import * as express from "express";
 import * as bodyParser from "body-parser";
+import { Routes } from "./routes/routes";
 
 // App class는 반드시 Singleton으로 해야 합니다.
 class App {
 
     public app: express.Application;
+    public routes: Routes = new Routes();
 
     constructor() {
         this.app = express();
-        this.config();        
+        this.config();
+        this.routes.routes(this.app);         
     }
 
     private config(): void{
@@ -20,4 +23,5 @@ class App {
 
 }
 
+// App은 여기서 한번 생성합니다. 다른 곳에서 절대 생성하지 마세요 방어코드 필요함
 export default new App().app;
