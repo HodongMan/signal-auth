2f71112e640550c84d78a9e54c0024c69069154d
hodongman <jhd9206@gmail.com>
2018-07-27 01:11:55 +0900

    routes comment and config update

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,5 +1,7 @@
 import {Request, Response} from "express";
 
+// 메인 라우터 입니다 
+
 export class Routes {       
     public routes(app): void {          
         app.route('/')
@@ -7,6 +9,15 @@ export class Routes {
             res.status(200).send({
                 message: 'main routes'
             })
-        })               
+        })
+        
+        /* 샘플입니다 아래에 이렇게 추가하세요 방어 코드 필요
+        app.route('/api')
+        .get((req: Request, res: Response) => {            
+            res.status(200).send({
+                message: 'main routes'
+            })
+        })
+        */
     }
 }
