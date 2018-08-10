a547242bc5bd85f3f2a82f18066b14c10f6cc2df
hodongman <arshavin3@naver.com>
2018-08-11 01:24:24 +0900

    user model update

--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -1,4 +1,5 @@
-import {Table, Column, Model, HasMany, DataType, Unique, PrimaryKey} from 'sequelize-typescript';
+import * as crypto from 'crypto';
+import {Table, Column, Model, HasMany, DataType, Unique, PrimaryKey, BeforeCount} from 'sequelize-typescript';
 
 @Table
 export class User extends Model<User> {
@@ -9,7 +10,7 @@ export class User extends Model<User> {
     @Column(DataType.STRING)
     @Unique
     @PrimaryKey
-    id: String;
+    readonly id: String;
 
     @Column(DataType.STRING)
     email: String;
@@ -22,4 +23,44 @@ export class User extends Model<User> {
    
     @UpdatedAt
     updated: Date;
+
+    @BeforeCreate
+
+    /**
+   * Make salt - db에 저장
+   *
+   * @param {Number} byteSize Optional salt byte size, default to 16
+   * @param {Function} callback
+   * @return {String}
+   * @api public
+   */
+
+    static makeSalt(byteSize: number, callback: Function): void {
+
+        const defaultByteSize: number = 16;
+
+        if ( typeof this.arguments[0] === 'function') {
+            callback = this.arguments[0];
+            byteSize = defaultByteSize;
+        } else if (typeof this.arguments[1] === 'function') {
+            callback = this.arguments[1];
+        }
+
+        if (byteSize < 1) {
+            byteSize = defaultByteSize;
+        }
+
+        if (!callback) {
+            //return crypto.randomBytes(byteSize).toString('base64');
+            return;
+        }
+
+        return crypto.randomBytes(byteSize, (err: Error, salt: Buffer): void => {
+            if (err) {
+                callback(err);
+              } else {
+                callback(null, salt.toString('base64'));
+              }
+        });
+    }
 }
